e008dbfbaac1dfae45678156734bd21759a081ec
thuc86 <phamngocthuc86@gmail.com>
2024-04-11 11:29:27 +0700

    Update HMI TCP client to test OTA HMI

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,38 @@
-use tokio::net::TcpStream;
+use std::env;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use serde_json::json;
+use tokio::net::TcpStream;
+use std::fs;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    // Connect to the server
-    let mut stream = TcpStream::connect("127.0.0.1:50123").await?;
+    // Parse command line arguments
+    let args: Vec<String> = env::args().collect();
 
-    // Prepare a JSON message to send
-    let msg = json!({
-        "msg_name": "download_msg",
-        "module": "hmi",
-        "percentage": 20,
-        "result": -2,
-    });
-
-    // let msg = json!({
-    //     "msg_name": "installing_msg",
-    //     "module": "hmi",
-    //     "percentage": 20,
-    //     "result": 0,
-    // });
-
-    // let msg = json!({
-    //     "msg_name": "cancel_msg",
-    //     "module": "hmi",
-    //     "percentage": 20,
-    //     "result": -2,
-    // });
-
-    
-    // let msg = json!({
-    //     "msg_name": "installing_done_msg",
-    //     "module": "hmi",
-    //     "result": 0,
-    // });
-
-
-    let msg = serde_json::to_string(&msg).unwrap();
-
-    // Send the JSON message
-    stream.write_all(msg.as_bytes()).await?;
-
-    loop {
-        // Wait for and read the response
-        let mut buffer = [0; 1024];
-        let n = stream.read(&mut buffer).await?;
-        // Assume the response is also a JSON message
-        if let Ok(str) = std::str::from_utf8(&buffer[..n]) {
-            println!("Received: {}", str);
-            stream.write_all(msg.as_bytes()).await?;
-        }
+    if args.len() < 3 || args[1] != "-f" {
+        println!("Usage: application -f <file_json_msg>");
+        return Ok(());
     }
 
+    // Extract the file path
+    let file_path = &args[2];
+
+    // Read the contents of the file
+    let file_content = fs::read_to_string(file_path)
+        .expect("Failed to read the file");
+
+    // Assuming the file content is directly the JSON message you want to send
+    // If the JSON needs to be nested or wrapped, you may need to adjust this
+
+    println!("Send JSON : File content: {}", file_content);
+    let json_msg: serde_json::Value = serde_json::from_str(&file_content)
+        .expect("File content is not valid JSON");
+
+    // Connect to the server
+    println!("Connecting to server at:  127.0.0.1:50123  - you can very by command: netstat -lnpt | grep 50123 ");
+    let mut stream = TcpStream::connect("127.0.0.1:50123").await?;
+
+    // Convert the JSON message to a string and send it
+    let msg_str = serde_json::to_string(&json_msg).unwrap();
+    stream.write_all(msg_str.as_bytes()).await?;
     Ok(())
 }
